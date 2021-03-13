45955aefd7e2eb4ee8dc5f4bde1c3d988f521821
unknown <chanhong9784@naver.com>
2021-03-13 16:19:36 +0900

    자연수 뒤집어 배열로 만들기

---
+++ Level1_29.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    string tmp = to_string(n);
+    
+    for(int i = tmp.size() - 1; i >= 0; i--)
+        answer.push_back(tmp[i]-'0');
+    return answer;
+}
