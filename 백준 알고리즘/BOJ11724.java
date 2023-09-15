3349712f4cc7f12ea8dd95884104e9e590c4e6aa
chanhong9784 <chanhong9784@naver.com>
2023-09-15 17:42:56 +0900

    [BOJ 11724] 연결 요소의 개수

---
+++ 백준 알고리즘/BOJ11724.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ11724 {
+	static ArrayList<Integer>[] list;
+	static boolean[] visited;
+	
+	static void dfs(int start) {
+		for(int l : list[start]) {
+			if(!visited[l]) {
+				visited[l] = true;
+				dfs(l);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int Ans = 0;
+		
+		list = new ArrayList[N + 1];
+		visited = new boolean[N + 1];
+		
+		for(int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			list[u].add(v);
+			list[v].add(u);
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			if(!visited[i]) {
+				dfs(i);
+				Ans++;
+			}
+		}
+		System.out.println(Ans);
+	}
+
+}
