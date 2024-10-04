a7c737639450760e2ca5830ef22dd9ca9b37630b
Chanhong Park <chanhong9784@naver.com>
2024-10-04 23:29:34 +0900

    [level 2] Title: 테이블 해시 함수, Time: 16.44 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        // 정렬
+        Arrays.sort(data, (o1, o2) -> {
+            if(o1[col - 1] == o2[col - 1]) {
+                return o2[0] - o1[0];
+            }
+            return o1[col - 1] - o2[col - 1];
+        });
+        
+        for(int i = row_begin - 1; i <= row_end - 1; i++) {
+            int[] t = data[i];
+            int mod = 0;
+            for(int j = 0; j < t.length; j++) {
+                mod += t[j] % (i + 1);
+            }
+            answer = answer^mod;
+        }
+    
+        return answer;
+    }
+}
